9a3a68d0b6dd3ff70604594a34196a63cfc6445d
Ai-Mei <purcia.princessmae296@gmail.com>
2023-05-14 14:18:31 +0800

    Class created.

--- CLASS-TV.py
+++ CLASS-TV.py
@@ -1,7 +1,11 @@
 # Create a TV class
-
+class TV:
 # Set the object's channel(integer), volume_level(integer), and if it is on or off(boolean).
-
+    def __init__(self, channel, volume_level, on):
+        self.channel = channel
+        self.volume_level = volume_level
+        self.on = on
+        
 # Create methods.
 
 # TurnOn()
