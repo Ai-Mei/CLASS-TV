18eeee8779776ffca25e3d781fd0e4ccf96821bb
Ai-Mei <purcia.princessmae296@gmail.com>
2023-05-14 14:15:40 +0800

    Psuedocode of the class

--- CLASS-TV.py
+++ CLASS-TV.py
@@ -1,1 +1,25 @@
-# Initial commit.
+# Create a TV class
+
+# Set the object's channel(integer), volume_level(integer), and if it is on or off(boolean).
+
+# Create methods.
+
+# TurnOn()
+
+# TurnOff()
+
+# GetChannel()
+
+# SetChannel()
+
+# GetVolume()
+
+# SetVolume()
+
+# ChannelUp()
+
+# ChannelDown()
+
+# VolumeUp()
+
+# VolumeDown()
